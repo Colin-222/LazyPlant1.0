c0c4cecf5f6f1d1d3387bbb53bcca0bd0e988030
Colin <Colin.liu22@gmail.com>
2019-08-28 19:05:16 +1000

    Change with tab view

--- app/src/main/java/com/example/lazyplant/AppDatabase.java
+++ app/src/main/java/com/example/lazyplant/AppDatabase.java
@@ -1,14 +1,14 @@
-//package com.example.lazyplant;
-//
-//import androidx.room.Database;
-//import androidx.room.RoomDatabase;
-//
-//import com.example.lazyplant.Favourite;
-//import com.example.lazyplant.FavouriteDAO;
-//
-//@Database(entities = {Favourite.class}, version = 1)
-//public abstract class AppDatabase extends RoomDatabase {
-//
-//    public abstract FavouriteDAO getFavouriteDAO();
-//
-//}
+package com.example.lazyplant;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.lazyplant.Favourite;
+import com.example.lazyplant.FavouriteDAO;
+
+@Database(entities = {Favourite.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract FavouriteDAO getFavouriteDAO();
+
+}
