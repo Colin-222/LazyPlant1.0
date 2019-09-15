bdd13e4cea93aaf73432f2e6f1ff347c3f9b718d
William Lu <wylu5@student.monash.edu>
2019-09-16 03:06:48 +1000

    Added notifications code to take to reminders, created reminders logic, changed navigation code. Modified Room database to get rid of some superfluos fields and added an id to PlantCareRecords.

--- app/src/androidTest/java/com/example/lazyplant/PlantDataTest.java
+++ app/src/androidTest/java/com/example/lazyplant/PlantDataTest.java
@@ -13,6 +13,7 @@ import com.example.lazyplant.plantdata.GardenPlant;
 import com.example.lazyplant.plantdata.GardenPlantDAO;
 import com.example.lazyplant.plantdata.PlantCareRecord;
 import com.example.lazyplant.plantdata.PlantCareRecordDAO;
+import com.example.lazyplant.ui.reminder.ReminderControl;
 
 import org.junit.After;
 import org.junit.Before;
@@ -32,6 +33,7 @@ public class PlantDataTest {
     private FavouriteDAO favouriteDAO;
     private PlantCareRecordDAO plantCareRecordDAO;
     private GardenPlantDAO gardenPlantDAO;
+    private ReminderControl reminderControl;
 
     @Before
     public void createDb() {
@@ -45,6 +47,7 @@ public class PlantDataTest {
         this.favouriteDAO.deleteAll();
         this.plantCareRecordDAO.deleteAll();
         this.gardenPlantDAO.deleteAll();
+        this.reminderControl = new ReminderControl(appContext, Constants.GARDEN_DB_TEST);
     }
 
     @Test
@@ -71,22 +74,31 @@ public class PlantDataTest {
     @Test
     public void PlantCareRecordDAO() throws Exception {
         PlantCareRecord x = new PlantCareRecord();
+        x.setWatering_id("A1");
         x.setPlant_id("A1");
         x.setEvent("BB");
         Calendar t1 = Calendar.getInstance();
         x.setDate(t1);
         PlantCareRecord x2 = new PlantCareRecord();
+        x2.setWatering_id("A2");
         x2.setPlant_id("A2");
         x2.setEvent("BB2");
         Calendar t2 = Calendar.getInstance();
         x2.setDate(t2);
+        PlantCareRecord x3 = new PlantCareRecord();
+        x3.setWatering_id("A3");
+        x3.setPlant_id("A2");
+        x3.setEvent("BB2");
+        x3.setDate(t2);
         this.plantCareRecordDAO.insert(x);
         this.plantCareRecordDAO.insert(x2);
-        assertEquals(2, this.plantCareRecordDAO.getPlantCareRecords().size());
+        this.plantCareRecordDAO.insert(x3);
+        assertEquals(3, this.plantCareRecordDAO.getPlantCareRecords().size());
         assertEquals(this.plantCareRecordDAO.getPlantCareRecord("A1").get(0).getDate(), t1);
         assertEquals(this.plantCareRecordDAO.getPlantCareRecord("A2").get(0).getDate(), t2);
+        assertEquals(2, this.plantCareRecordDAO.getPlantCareData("A2").size());
         this.plantCareRecordDAO.deleteFavourite("A1");
-        assertEquals(1, this.plantCareRecordDAO.getPlantCareRecords().size());
+        assertEquals(2, this.plantCareRecordDAO.getPlantCareRecords().size());
         this.plantCareRecordDAO.deleteAll();
     }
 
@@ -95,31 +107,73 @@ public class PlantDataTest {
         GardenPlant x = new GardenPlant();
         Calendar t1 = Calendar.getInstance();
         x.setPlant_id("A1");
-        x.setActive(true);
         x.setNotes("N1");
         x.setWatering_interval(5);
         x.setLast_watering(t1);
         GardenPlant x2 = new GardenPlant();
         Calendar t2 = Calendar.getInstance();
         x2.setPlant_id("A2");
-        x2.setActive(false);
         x2.setWatering_interval(7);
         x2.setLast_watering(t2);
         GardenPlant x3 = new GardenPlant();
         x3.setPlant_id("A3");
-        x3.setActive(true);
         x3.setWatering_interval(5);
         x3.setLast_watering(t1);
         this.gardenPlantDAO.insert(x);
         this.gardenPlantDAO.insert(x2);
         this.gardenPlantDAO.insert(x3);
-        assertEquals(2, this.gardenPlantDAO.getActiveGardenPlants().size());
+        assertEquals(3, this.gardenPlantDAO.getGardenPlants().size());
         assertEquals(this.gardenPlantDAO.getGardenPlant("A1").get(0).getNotes(), "N1");
         assertEquals(t2, this.gardenPlantDAO.getGardenPlant("A2").get(0).getLast_watering());
         x.setNotes("N2");
         this.gardenPlantDAO.update(x);
         assertEquals(this.gardenPlantDAO.getGardenPlant("A1").get(0).getNotes(), "N2");
         this.gardenPlantDAO.deleteAll();
+    }
+
+    @Test
+    public void reminder() throws IOException {
+        String i1 = this.reminderControl.addGardenPlant("AAA", null);
+        String i2 = this.reminderControl.addGardenPlant("BBB", null);
+        String i3 = this.reminderControl.addGardenPlant("CCC", null);
+        assertEquals(3, this.gardenPlantDAO.getGardenPlants().size());
+        this.reminderControl.recordPlantWatering(i1, Constants.WATERING);
+        this.reminderControl.recordPlantWatering(i3, Constants.WATERING);
+        assertEquals(2, this.plantCareRecordDAO.getPlantCareRecords().size());
+        this.reminderControl.editNotes(i2, "BLA");
+        assertEquals("BLA", this.reminderControl.getGardenPlant(i2).getNotes());
+        this.gardenPlantDAO.deleteAll();
+
+        Calendar t = Calendar.getInstance();
+        Calendar c5 = Calendar.getInstance();
+        Calendar c7 = Calendar.getInstance();
+
+        GardenPlant x = new GardenPlant();
+        x.setPlant_id("A1");
+        x.setNotes("N1");
+        x.setWatering_interval(5);
+        x.setLast_watering(t);
+        GardenPlant x2 = new GardenPlant();
+        x2.setPlant_id("A2");
+        x2.setWatering_interval(7);
+        x2.setLast_watering(t);
+        GardenPlant x3 = new GardenPlant();
+        x3.setPlant_id("A3");
+        x3.setWatering_interval(5);
+        x3.setLast_watering(t);
+        this.gardenPlantDAO.insert(x);
+        this.gardenPlantDAO.insert(x2);
+        this.gardenPlantDAO.insert(x3);
+
+        c5.add(Calendar.DATE, 5);
+        c7.add(Calendar.DATE, 7);
+        List <GardenPlant> lgp0 = this.reminderControl.getPlantsToWater(t);
+        List <GardenPlant> lgp5 = this.reminderControl.getPlantsToWater(c5);
+        List <GardenPlant> lgp7 = this.reminderControl.getPlantsToWater(c7);
+        assertEquals(0, lgp0.size());
+        assertEquals(2, lgp5.size());
+        assertEquals(1, lgp7.size());
+
     }
 
 
---
+++ app/src/main/java/com/example/lazyplant/AlarmBroadcastReceiver.java
@@ -0,0 +1,73 @@
+package com.example.lazyplant;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Calendar;
+
+public class AlarmBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        showNotification(context);
+    }
+
+    void showNotification(Context context) {
+        String CHANNEL_ID = "LazyPlantChannel";// The id of the channel.
+        CharSequence name = context.getResources().getString(R.string.app_name);// The user-visible name of the channel.
+        NotificationCompat.Builder mBuilder;
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(Constants.NOTIFICATION_TAG, Constants.REMINDER);
+        notificationIntent.putExtras(bundle);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= 26) {
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
+            mNotificationManager.createNotificationChannel(mChannel);
+            mBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setLights(Color.RED, 300, 300)
+                    .setChannelId(CHANNEL_ID)
+                    .setContentTitle("LazyPlant Watering Reminder");
+        } else {
+            mBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setContentTitle("LazyPlant Watering Reminder");
+        }
+
+        mBuilder.setContentIntent(contentIntent);
+        mBuilder.setContentText("Your plants need watering. Click here to view.");
+        mBuilder.setAutoCancel(true);
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
+    public static void startAlarmBroadcastReceiver(Context context) {
+        Intent _intent = new Intent(context, AlarmBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, _intent, 0);
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis() + 1000);
+        /*calendar.set(Calendar.HOUR_OF_DAY, 22);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 0);*/
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
+
+
+}
