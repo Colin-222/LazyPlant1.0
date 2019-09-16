91545c8b82b892be8f2a61a645fab6632c4a90ef
Colin <Colin.liu22@gmail.com>
2019-09-16 12:28:16 +1000

    Revert "Change with tab view"
    This reverts commit ad46a416d6c32f882b4a0fe2b296d6caa37caa2b.
    
    # Conflicts:
    #	app/src/main/java/com/example/lazyplant/ui/search/SearchFragment.java
    #	app/src/main/res/layout/fragment_search.xml

---
+++ app/src/main/java/com/example/lazyplant/StoreMap.java
@@ -0,0 +1,104 @@
+package com.example.lazyplant;
+
+import java.util.ArrayList;
+
+
+public class StoreMap {
+
+    ArrayList<StoreLocationData> locationList = new ArrayList<StoreLocationData>();
+
+    public void main(String[] args) {
+
+        locationList.add(new StoreLocationData("Altona Bunnings Warehouse", "Corner Millers and Ross Road \n" +
+                "Altona VIC 3018", -37.846360, 144.845750));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" +
+                "Bairnsdale VIC 3875", -37.836201,147.599747));
+        locationList.add(new StoreLocationData("Ballarat Bunnings Warehouse", "306-312 Creswick Road \n" +
+                "Ballarat VIC 3350", -37.551739, 143.851776));
+        locationList.add(new StoreLocationData("Bayswater Bunnings Warehouse", "183-225 Canterbury Road \n" +
+                "Bayswater VIC 3153", -37.829160, 145.279230));
+        locationList.add(new StoreLocationData("Bendigo Bunnings Warehouse", "263-265 High Street \n" +
+                "Kangaroo Flat VIC 3555", -36.808420, 144.241190));
+        locationList.add(new StoreLocationData("Box Hill Bunnings Warehouse", "259 Middleborough Road \n" +
+                "Box Hill VIC 3128", -37.836970, 145.135150));
+        locationList.add(new StoreLocationData("Broadmeadows Bunnings Warehouse", "Homemaker Centre, 100 Pearcedale Parade \n" +
+                "Broadmeadows VIC 3047", -37.674850, 144.919670));
+        locationList.add(new StoreLocationData("Brunswick Bunnings Warehouse", "415 Sydney Road \n" +
+                "Brunswick VIC 3056", -37.767550, 144.961830));
+        locationList.add(new StoreLocationData("Caroline Springs Bunnings Warehouse", "1067- 1125 Western Highway \n" +
+                "Caroline Springs VIC 3023", -37.755580, 144.738650));
+        locationList.add(new StoreLocationData("Carrum Downs Bunnings Warehouse", "600 Frankston-Dandenong Road \n" +
+                "Carrum Downs VIC 3201", -38.093360, 145.177540));
+        locationList.add(new StoreLocationData("Chadstone Bunnings Warehouse", "Homemaker Centre, 675-685 Warrigal Rd \n" +
+                "Chadstone VIC 3148", -37.877750, 145.091300));
+        locationList.add(new StoreLocationData("Coburg Bunnings Warehouse", "64-96 Gaffney Street \n" +
+                "Coburg VIC 3058", -37.733260, 144.959240));
+        locationList.add(new StoreLocationData("Colac Bunnings Warehouse", "130-138 Bromfield Street \n" +
+                "Colac VIC 3250", -38.340970, 143.591450));
+        locationList.add(new StoreLocationData("Collingwood Bunnings Warehouse", "179-201 Victoria Parade \n" +
+                "Collingwood VIC 3066", -37.809292, 144.989807));
+        locationList.add(new StoreLocationData("Craigieburn Bunnings Warehouse", "700 Hume Highway \n" +
+                "Craigieburn VIC 3064", -37.594158, 144.942856));
+        locationList.add(new StoreLocationData("Cranbourne Bunnings Warehouse", "1205 Thompsons Road (Corner South Gippsland Highway) \n" +
+                "Cranbourne VIC 3977", -38.082240, 145.276340));
+        locationList.add(new StoreLocationData("Croydon Bunnings Warehouse", "268-288 Maroondah Highway \n" +
+                "Chirnside Park VIC 3116", -37.766810, 145.303030));
+        locationList.add(new StoreLocationData("Dandenong Bunnings Warehouse", "101 Princes Hwy \n" +
+                "Dandenong South VIC 3175", -38.003700, 145.249110));
+        locationList.add(new StoreLocationData("Delacombe Bunnings Warehouse", "307 Smythes Rd \n" +
+                "Delacombe VIC 3356", -37.590610, 143.807740));
+        locationList.add(new StoreLocationData("East Pakenham Bunnings Warehouse", "56-58 Bald Hill Rd \n" +
+                "Pakenham VIC 3810", -38.084410, 145.493770));
+        locationList.add(new StoreLocationData("Echuca Bunnings Warehouse", "131 Ogilvie Avenue \n" +
+                "Echuca VIC 3564", -36.140730, 144.757060));
+        locationList.add(new StoreLocationData("Eltham Bunnings Warehouse", "Corner Bridge & Silver Street \n" +
+                "Eltham VIC 3095", -37.718208, 145.139893));
+        locationList.add(new StoreLocationData("Epping Bunnings Warehouse", "310 Cooper Street \n" +
+                "Epping VIC 3076", -37.649310, 145.003820));
+        locationList.add(new StoreLocationData("Epsom Bunnings Warehouse", "Corner Midland Highway & Toma Court \n" +
+                "Epsom VIC 3551", -36.710830, 144.315010));
+        locationList.add(new StoreLocationData("Fountain Gate Bunnings Warehouse", "64-86 Narre Warren North Road \n" +
+                "Fountain Gate VIC 3804", -38.021172, 145.308151));
+        locationList.add(new StoreLocationData("Frankston Bunnings Warehouse", "Corner McMahon & Gertrude Street \n" +
+                "Frankston VIC 3199", -38.149350, 145.136110));
+        locationList.add(new StoreLocationData("Fairfield Bunnings Warehouse", "266 Darebin Road \n" +
+                "Fairfield VIC 3078", -37.765870, 145.022600));
+        locationList.add(new StoreLocationData("Geelong North Bunnings Warehouse", "5-19 Princes Highway \n" +
+                "Norlane VIC 3214", -38.097680, 144.354880));
+        locationList.add(new StoreLocationData("Hastings Bunnings Warehouse", "1837 Frankston-Flinders Road \n" +
+                "Hastings VIC 3915", -38.285120, 145.183900));
+        locationList.add(new StoreLocationData("Hawthorn Bunnings Warehouse", "230 Burwood Road \n" +
+                "Hawthorn VIC 3122", -37.822320, 145.031510));
+        locationList.add(new StoreLocationData("Hoppers Crossing Bunnings Warehouse", "221-231 Old Geelong Road \n" +
+                "Hoppers Crossing VIC 3029", -37.876800, 144.712930));
+        locationList.add(new StoreLocationData("Horsham Bunnings Warehouse", "24-38 Wilson Street \n" +
+                "Horsham VIC 3400", -36.716440, 142.201640));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+        locationList.add(new StoreLocationData("Bairnsdale Bunnings Warehouse", "485-487 Main Street \n" + "Bairnsdale VIC 3875", -37.836200, 147.599740));
+
+
+
+    }
+}
