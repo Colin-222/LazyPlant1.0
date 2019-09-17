a61a1eb7c2c191ad4bca976404396f3a72e2e660
Colin <Colin.liu22@gmail.com>
2019-09-18 01:29:31 +1000

    Merge branch 'noColin' of https://github.com/Colin-222/LazyPlant1.0
    # Conflicts:
    #	app/src/main/java/com/example/lazyplant/ui/DisplayHelper.java
    #	app/src/main/java/com/example/lazyplant/ui/favourites/FavouritesFragment.java
    #	app/src/main/java/com/example/lazyplant/ui/plantListDisplayHelper.java
    #	app/src/main/res/navigation/mobile_navigation.xml

--- app/src/main/java/com/example/lazyplant/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lazyplant/ui/home/HomeFragment.java
@@ -19,6 +19,8 @@ import com.example.lazyplant.R;
 import com.example.lazyplant.plantdata.AppDatabase;
 import com.example.lazyplant.plantdata.Favourite;
 import com.example.lazyplant.plantdata.FavouriteDAO;
+import com.example.lazyplant.shopmap.ShopMapActivity;
+import com.example.lazyplant.shopmap.StoreMap;
 import com.example.lazyplant.ui.plantDetails.PlantDetailsActivity;
 
 import java.util.List;
@@ -28,8 +30,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        
 
+        Button shopLocationButton = (Button) getView().findViewById(R.id.shop_location_button);
+        shopLocationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ShopMapActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
 
