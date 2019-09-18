221033defed71eba7c0d9edf301679e075afeed2
Colin <Colin.liu22@gmail.com>
2019-09-18 23:13:24 +1000

    Merge branch 'noColin' of https://github.com/Colin-222/LazyPlant1.0
    # Conflicts:
    #	app/src/main/java/com/example/lazyplant/ui/DisplayHelper.java
    #	app/src/main/java/com/example/lazyplant/ui/favourites/FavouritesFragment.java
    #	app/src/main/java/com/example/lazyplant/ui/plantListDisplayHelper.java
    #	app/src/main/res/navigation/mobile_navigation.xml

---
+++ app/src/main/java/com/example/lazyplant/ui/shopmap/StoreLocationData.java
@@ -0,0 +1,48 @@
+package com.example.lazyplant.ui.shopmap;
+
+public class StoreLocationData {
+    private String storeName;
+    private String storeAddress;
+    private Double storeLatitude;
+    private Double storeLongitude;
+
+    public StoreLocationData(String name, String address, Double latitude, Double longitude) {
+        this.storeName = name;
+        this.storeAddress = address;
+        this.storeLatitude = latitude;
+        this.storeLongitude = longitude;
+    }
+
+    public Double getStoreLatitude() {
+        return storeLatitude;
+    }
+
+    public Double getStoreLongitude() {
+        return storeLongitude;
+    }
+
+    public String getStoreAddress() {
+        return storeAddress;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreAddress(String storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public void setStoreLatitude(Double storeLatitude) {
+        this.storeLatitude = storeLatitude;
+    }
+
+    public void setStoreLongitude(Double storeLongitude) {
+        this.storeLongitude = storeLongitude;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+}
